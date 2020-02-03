73c56c29a29a00aacfa12f9b4f12ff3c475d00b6
notesonartificialintelligence <59626140+notesonartificialintelligence@users.noreply.github.com>
2020-02-03 21:06:24 +0000

    Chapter 1 - Python Basics
    I've already covered most of the things in this chapter, as such, I've created a few examples without the aid of the author. The practice questions were also answered.

---
+++ Chapter 01 - Python Basics/notes_on_chapter_1.py
@@ -0,0 +1,22 @@
+#Gabriel Abraham
+#notesonartificialintelligence
+#Automate The Borning Stuff With Python - Chapter 1
+#I've covered most of these things alreaady; as such I'll create the example without using the code of the #author. If something is to be learnt; I'll implement it.
+
+#This progam says hello, and asks for my name.
+
+print("Hello\nWhat is your name.")
+name = input()
+
+print(f"Its good to meet you, {name}")
+print("The length of your name is: ")
+length = len(name)
+
+print(name)
+
+print("What is your age?")
+age = input()
+print(f"You\'ll be {str(int(age) + 1)}  in a year.")
+
+1dd = 3
+print(1dd)
