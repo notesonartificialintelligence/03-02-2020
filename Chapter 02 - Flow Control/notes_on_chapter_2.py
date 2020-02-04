89167b521bb7bb2be3ff1ef25d2428b7482e6d44
notesonartificialintelligence <59626140+notesonartificialintelligence@users.noreply.github.com>
2020-02-04 22:53:19 +0000

    Chapter 02 - Flow Control

---
+++ Chapter 02 - Flow Control/notes_on_chapter_2.py
@@ -0,0 +1,32 @@
+#Gabriel Abraham
+#notesonartificialintelligence
+#Automate The Borning Stuff With Python - Chapter 2
+#I've covered most of these things alreaady; as such I'll create the example without using the code of the #author. If something is to be learnt; I'll implement it.
+
+#Break Statements allows the program execution to break out of a while loop.
+
+
+while True:
+	print("Please type your name.")
+	name = input()
+	
+	if name.lower() == "tim":
+		break
+print("Thank you!")
+
+#Continue Statements; these statements are used inside loops, when it's reached the program
+#execution immediately jumps back to the start of the loop, and reevaluated the loops condition.
+
+while True:
+	print("Who are you?")
+	name = input()
+	if name != "Joe":
+		continue
+	print("Hello, Joe. What is the password? (It is a fish.)")
+	password = input()
+	if password == "swordfish":
+		break
+print("Access granted.")
+
+#If the users name is not the same as joe, the program flow will immdeiately jump to the start
+#of the loop.
